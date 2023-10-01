250441909989c8f14717696a5be4f21b5ed1c35c
LUCAS MARTINEZ <132151483+LucasFrontEnd21122007@users.noreply.github.com>
2023-10-01 19:34:12 -0300

    commits
    arquivos do projeto

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+
+        if (taskText !== "") {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                ${taskText}
+                <button class="deleteTask">Excluir</button>
+            `;
+            taskList.appendChild(li);
+            taskInput.value = "";
+        }
+    });
+
+    taskList.addEventListener("click", function (e) {
+        if (e.target.classList.contains("deleteTask")) {
+            e.target.parentElement.remove();
+        }
+    });
+});
